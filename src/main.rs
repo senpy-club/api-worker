2196bd82ce7f23cd8c9b83482f616ddb72dbfcc8
Fuwn <contact@fuwn.me>
2021-04-28 12:56:52 +0000

    fix?: CORS

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,31 @@ pub mod routes;
 pub mod structures;
 pub mod utils;
 
+use rocket_cors as cors;
+
 #[launch]
 fn rocket() -> _ {
   dotenv::dotenv().ok();
 
-  rocket::build().mount("/", routes![routes::index]).mount(
-    "/api/v1",
-    routes![
-      routes::github,
-      routes::languages,
-      routes::language,
-      routes::random
-    ],
-  )
+  rocket::build()
+    .manage(
+      cors::CorsOptions {
+        allowed_origins: cors::AllowedOrigins::all(),
+        allowed_methods: cors::AllowedMethods::new(),
+        allowed_headers: cors::AllowedHeaders::all(),
+        ..Default::default()
+      }
+      .to_cors()
+      .unwrap(),
+    )
+    .mount("/", routes![routes::index])
+    .mount(
+      "/api/v1",
+      routes![
+        routes::github,
+        routes::languages,
+        routes::language,
+        routes::random
+      ],
+    )
 }
