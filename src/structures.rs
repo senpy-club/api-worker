abbbe893905b52d75b54da1859de7316a0224ce2
Fuwn <fuwnzy@gmail.com>
2022-03-07 21:39:02 -0800

    chore: licensing

--- src/structures.rs
+++ src/structures.rs
@@ -1,4 +1,19 @@
-// Copyright (C) 2021-2021 the senpy club
+// This file is part of api-worker <https://github.com/senpy-club/api-worker>.
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+//
+// Copyright (C) 2022-2022 Fuwn <contact@fuwn.me>
 // SPDX-License-Identifier: GPL-3.0-only
 
 #![allow(clippy::used_underscore_binding)]
