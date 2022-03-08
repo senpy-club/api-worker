1535e729abb4d7f634ba079ba412b6d90d5245d8
Fuwn <fuwnzy@gmail.com>
2022-03-07 21:32:25 -0800

    chore(nix): remove

--- nix/rust.nix
+++
@@ -1,9 +0,0 @@
-{ sources ? import ./sources.nix }:
-
-let
-  pkgs = import sources.nixpkgs { overlays = [ (import sources.nixpkgs-mozilla) ]; };
-  channel = "nightly";
-  date = "2021-04-24";
-  targets = [ ];
-  chan = pkgs.rustChannelOfTargets channel date targets;
-in chan
